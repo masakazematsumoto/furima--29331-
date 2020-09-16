18fee8ce72b26519e2b5a2fd4215c79afa3e07fb
masakazematsumoto <kazecocoa0919@gmail.com>
2020-09-16 21:04:15 +0900

    単体テストコード　修正2

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name { "画像" }
     description { "背景画像" }
-    category_id { 1 }
-    condition_id { 1 }
-    postage_id { 1 }
-    prefecture_id { 1 }
-    handling_time_id { 1 }
-    price { 1000 }
+    category_id { '2' }
+    condition_id { '2' }
+    postage_id { '2' }
+    prefecture_id { '2' }
+    handling_time_id { '2' }
+    price { '1000' }
     association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 describe Item do
   before do
-    @user = FactoryBot.build(:item)
-    @item.image = fixture_file_upload(‘sample_test.png’)
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('sample_test.png')
   end
 
   describe "商品出品情報の登録" do
     context "商品出品情報が保存できる時" do
 
-      it "image, name, description, category_id, condition_id, postage_id, prefecture_id, handling_time_id, priceが存在すれば保存できること" do
+      it "name, description, category_id, condition_id, postage_id, prefecture_id, handling_time_id, priceが存在すれば保存できること" do
         expect(@item).to be_valid
+
       end
     end
 
@@ -38,97 +39,97 @@ describe Item do
       it "category_idが空では保存できない" do
         @item.category_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category Select")
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
       it "category_idをid:1を選択していると保存できない" do
         @item.category_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category Select")
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
       it "condition_idが空では保存できない" do
         @item.condition_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Condition Select")
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
 
       it "condition_idをid:1を選択していると保存できない" do
         @item.condition_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Condition Select")
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
 
       it "postage_idが空では保存できない" do
         @item.postage_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Postage select")
+        expect(@item.errors.full_messages).to include("Postage can't be blank")
       end
 
       it "postage_idをid:1を選択していると保存できない" do
         @item.postage_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Postage select")
+        expect(@item.errors.full_messages).to include("Postage can't be blank")
       end
 
       it "prefecture_idが空では保存できない" do
         @item.prefecture_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture select")
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
 
       it "prefecture_idをid:1を選択していると保存できない" do
         @item.prefecture_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture select")
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
 
       it "prefecture_idが空では保存できない" do
         @item.prefecture_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture select")
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
 
       it "prefecture_idをid:1を選択していると保存できない" do
         @item.prefecture_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture select")
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
 
       it "handling_time_idが空では保存できない" do
         @item.handling_time_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("HandlingTime select")
+        expect(@item.errors.full_messages).to include("Handling time can't be blank")
       end
 
       it "handling_time_idをid:1を選択していると保存できない" do
         @item.handling_time_id = "1"
         @item.valid?
-        expect(@item.errors.full_messages).to include("HandlingTime select")
+        expect(@item.errors.full_messages).to include("Handling time can't be blank")
       end
 
       it "priceが空では保存できない" do
         @item.price = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank", "Price Half-width number", "Price Out of setting range")
+        expect(@item.errors.full_messages).to include("Price can't be blank", "Price is invalid. Input half-width characters.", "Price is not a number")
       end
 
       it "priceが300円より小さいと保存できない" do
         @item.price = "299"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Out of setting range")
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
 
       it "priceが9,999,999円より大きいと保存できない" do
         @item.price = "10,000,000"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Out of setting range")
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
 
       it "priceが半角数字でないと保存できない" do
         @item.handling_time_id = "１０００"
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Half-width number", "Price Out of setting range")
+        expect(@item.errors.full_messages).to include("Handling time can't be blank")
       end
     end
   end
