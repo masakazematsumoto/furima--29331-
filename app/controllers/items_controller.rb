ea0b73f2509f07d51881dab2dcf43a06b2f23bde
masakazematsumoto <kazecocoa0919@gmail.com>
2020-09-21 18:25:55 +0900

    エラーハンドリング実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,14 +29,18 @@ class ItemsController < ApplicationController
   
   def update
     @item = Item.find(params[:id])
-    @item.update!(item_params)
-    redirect_to item_path
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
   
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :postage_id, :condition_id, :prefecture_id, :handling_time_id, :price, :image).merge(user_id: current_user.id)
   end
-  
+
 end
 
 
