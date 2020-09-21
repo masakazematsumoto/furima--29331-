da6b259f866a52ae6966a7e5efeac3e694757dc2
masakazematsumoto <kazecocoa0919@gmail.com>
2020-09-21 19:01:35 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit, :update]
   
   def index
     @items = Item.all.order(id: "DESC")
@@ -20,17 +21,17 @@ class ItemsController < ApplicationController
 
   
   def show
-    @item = Item.find(params[:id])
+    
   end
   
   def edit
-    @item = Item.find(params[:id])
+    
   end
   
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
+    
+    
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
@@ -39,6 +40,10 @@ class ItemsController < ApplicationController
   
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :postage_id, :condition_id, :prefecture_id, :handling_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
