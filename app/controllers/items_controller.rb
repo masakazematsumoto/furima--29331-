e7ac1f1e2467ba90dc6c00ca9d4fdb5772acab2a
masakazematsumoto <kazecocoa0919@gmail.com>
2020-09-21 18:04:27 +0900

    商品情報編集 first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,18 +18,25 @@ class ItemsController < ApplicationController
     end
   end
 
-  def item_params
-    params.require(:item).permit(:name, :description, :category_id, :postage_id, :condition_id, :prefecture_id, :handling_time_id, :price, :image).merge(user_id: current_user.id)
-  end
-
+  
   def show
     @item = Item.find(params[:id])
   end
-
-  #def edit
-    #@item = Item.find(params[:id])
-  #end
-
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    @item.update!(item_params)
+    redirect_to item_path
+  end
+  
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :postage_id, :condition_id, :prefecture_id, :handling_time_id, :price, :image).merge(user_id: current_user.id)
+  end
+  
 end
 
 
