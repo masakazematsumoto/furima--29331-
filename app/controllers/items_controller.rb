886a78f3408f2f2bd4cd3fc5c5f8f7cebfca00b6
masakazematsumoto <kazecocoa0919@gmail.com>
2020-09-21 14:57:42 +0900

    詳細表示 first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,14 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :description, :category_id, :postage_id, :condition_id, :prefecture_id, :handling_time_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
 end
 
 
