6fed5c82e92f55f86cfb75522aa4336b54670578
masakazematsumoto <kazecocoa0919@gmail.com>
2020-09-26 19:41:17 +0900

    テストコード記述

--- spec/factories/order_donations.rb
+++ spec/factories/order_donations.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
-  factory :order do
+  factory :order_donation do
     post_code { "555-0000"}
     prefecture_id { "1" }
     city { "大阪市" }
     address { "1-2-3" }
     building_name { "大阪ビル" }
     phone_number { "09012345678" }
+    token {}
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Item do
+
+  before do
+    @order = FactoryBot.build(:order)
+    @order.image = fixture_file_upload('sample_test.png')
+  end
+
+  describe "住所情報の登録" do
+    context "住所情報が保存できる時" do
+
+      it "post_code, prefecture_id, city, address, building_name, phone_numberが存在すれば保存できること" do
+        expect(@order).to be_valid
+      end
+
+    end
+  end
+
+
+
+
+
+
+
+
+
 end
