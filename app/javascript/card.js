d65f8c8ad1dda0c5fdb79646b45da1c2cf8f005d
masakazematsumoto <kazecocoa0919@gmail.com>
2020-09-28 16:45:26 +0900

    エラーハンドリング修正1

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,18 +8,18 @@ const pay = () => {
   const formResult = document.getElementById("charge-form");
   const formData = new FormData(formResult);
 
-  const creditCardForm = { // カードオブジェクトを生成
+  const card = { // カードオブジェクトを生成
       number: formData.get("order_donation[number]"),              // カード番号
       cvc: formData.get("order_donation[cvc]"),                    // カード裏面の3桁の数字
       exp_month: formData.get("order_donation[exp_month]"),        // 有効期限の月
       exp_year: `20${formData.get("order_donation[exp_year]")}`,   // 有効期限の年
     };
     //debugge
-    Payjp.createToken(creditCardForm, (status, response) => {
+    Payjp.createToken(card, (status, response) => {
       if (status === 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");   //idを元に要素を取得
-        const tokenObj = `<input value=${token} type="hidden" name='card_token'>`;   //paramsの中にトークンを含める
+        const tokenObj = `<input value=${token} type="hidden" name='authenticity_token'>`;   //paramsの中にトークンを含める
         renderDom.insertAdjacentHTML("beforeend", tokenObj);  //フォームの一番最後に要素を追加
 
         document.getElementById("card-number").removeAttribute("name");
@@ -30,8 +30,12 @@ const pay = () => {
         document.getElementById("charge-form").submit();
         document.getElementById("charge-form").reset();
       }   
+      else {
+        render 'shared/error_messages', model: f.object
+      }
     });
   });
 };
+
 
 window.addEventListener("load", pay);
